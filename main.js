a73a8cfda12bc7375f830364c062865d1938f886
Mantosh Sah <59989979+mantu1738@users.noreply.github.com>
2021-03-17 01:25:00 +0530

    full css done parallax effect added

---
+++ main.js
@@ -0,0 +1,14 @@
+$(window).scroll(function() {
+    parallax();
+});
+
+function parallax() {
+    var windowScroll = $(window).scrollTop();
+    // console.log(windowScroll);
+
+    $('.parallax-effect').css('background-position',
+     'center ' +(windowScroll * 0.75)+'px');
+
+     $('.parallax-box').css('top', 
+     -5+(windowScroll * 0.008)+'em');
+}
