fc72c5b091e75567e649aaf064dfab70e0d43042
Mantosh Sah <59989979+mantu1738@users.noreply.github.com>
2021-03-17 12:24:13 +0530

    logo gif added

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function parallax() {
     // console.log(windowScroll);
 
     $('.parallax-effect').css('background-position',
-     'center ' +(windowScroll * 0.75)+'px');
+     'center ' +(windowScroll  * 0.75)+'px');
 
      $('.parallax-box').css('top', 
      -5+(windowScroll * 0.008)+'em');
